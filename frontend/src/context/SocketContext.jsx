a4f568710bfca726b9fb128b9648cb14562e2feb
nikhilesh36 <nikhileshk4545@gmail.com>
2025-01-11 00:06:30 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(()=>{
         if(authUser){
-            const socket=io("http://localhost:2000",{
+            const socket=io("https://chat-application-mepc.onrender.com",{
                 query:{
                     userId:authUser._id,
                 }
